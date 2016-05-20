73e6e4d7a87ab94d175e1977a0c62a47af32451a
Blakhard <qwebek@mail.ru>
2016-05-20 21:57:29 +0300

    Added npm configs

---
+++ web/src/main/resources/static/webpack.config.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var autoprefixer = require('autoprefixer');
+var webpack = require('webpack');
+module.exports = {
+    context: __dirname + '/src',
+    entry: {
+        'js/app':"./scripts/app.js",
+        'style/style':"./style/style.scss"
+    },
+    output: {
+        path: __dirname + '/public',
+        filename: "[name].js"
+    },
+    resolve:{
+      extensions: ['', '.js', '.scss']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loaders: ["react-hot", "babel?presets[]=react,presets[]=es2015,plugins[]=transform-object-rest-spread"]
+            },
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract("css!postcss!sass")
+            }
+        ]
+    },
+    postcss:[ autoprefixer({browsers: ['last 2 versions']})],
+    resolveLoader:{
+        modulesDirectories: ['node_modules'],
+        moduleTemplates: ['*-loader','*'],
+        extensions: ['','.js']
+    },
+    plugins: [
+        new ExtractTextPlugin('[name].css', {allChunks: true})
+    ],
+    watch: true
+
+
+};
